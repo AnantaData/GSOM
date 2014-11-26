c533b058df675161162f5e23c011e59864eed8f3
Lakmal <tmtlakmal@gmail.com>
2014-11-26 10:03:37 +0530

    structured the code of machine fusion

--- machinefusion.py
+++ machinefusion.py
@@ -1,13 +1,26 @@
+from sklearn.cross_validation import train_test_split
+
 __author__ = 'lakmal/laksheen'
 
+#from sklearn import cross_validation
+from sklearn.utils import resample
 #data - the data set , gsom - the gsom to be  used
 
 def _get_bootstrap_samples(data):
     bootstrap_data = []
+    #bs = cross_validation.Bootstrap(9, random_state=0)
     return bootstrap_data
 
-def _get_trained_models(gsom, bootstrap_data):
+def _get_trained_models(gsom, no_samples, features):
     trained_gsoms = []
+    #bs = cross_validation.Bootstrap(no_samples, random_state=0)
+    #bs = resample(features, n_samples=no_samples, random_state=0)
+    #print bs
+    i=0
+    for train_index, test_index in range(0,no_samples):
+        features_train, features_test = train_test_split(features, test_size=0.33, random_state=42)
+        trained_gsoms.append(gsom)
+        trained_gsoms[i].process_batch(features[train_index],750)
     return trained_gsoms
 
 def _get_coassoc_vec(trained_gsoms):
@@ -40,3 +53,26 @@ def machine_fusion_gsom(data, gsom):
     codebook_groups     = _group_codebooks(incident_mat)
     return _get_fused_gsom(codebook_groups)
 
+
+import numpy as np
+from gsom import gsomap
+import matplotlib.pyplot as plt
+data = np.loadtxt("zoo.data.txt",dtype=str,delimiter=",")
+
+data = np.array(data)
+
+names = data[:,0]
+
+names= np.column_stack((names,data[:,-1]))
+
+features= data[:,:-1]
+features = features[:,1:].astype(int)
+
+
+print features.shape
+
+positions = np.ndarray(shape=(101,2))
+
+gsom = gsomap(SP=0.9,dims=16,nr_s=4,lr_s=0.9,fd=0.99999)
+
+print _get_trained_models(gsom, 10,features)
